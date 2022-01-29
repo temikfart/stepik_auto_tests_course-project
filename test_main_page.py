74dcedb57bfe3201cde7493808b7934c15bc9fa5
temikfart <fartygin.a@phystech.edu>
2022-01-29 21:37:00 +0300

    complete checks on login page in tests.

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
 
 
@@ -6,9 +7,11 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook
 
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
