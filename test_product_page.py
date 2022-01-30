4af67d239d6c320c6812fdd880614c728bf35b21
temikfart <fartygin.a@phystech.edu>
2022-01-30 13:38:14 +0300

    add marker 'need_review' to some tests.

--- test_product_page.py
+++ test_product_page.py
@@ -8,6 +8,7 @@ from pages.basket_page import BasketPage
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -42,6 +43,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -50,6 +52,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page.should_be_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -74,6 +77,7 @@ class TestUserAddToBasketFromProductPage():
         self.login_page.should_be_authorized_user()
         self.link = link
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, self.link)
         page.open()
